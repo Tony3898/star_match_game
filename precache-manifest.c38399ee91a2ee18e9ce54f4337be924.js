d1f278d1dd5945770c447fa774eb508a6b40eda1
Tony3898 <Tony3898@users.noreply.github.com>
2020-10-20 18:56:52 +0000

    deploy: 9aabe4a1a513ee0c31cd1574d182130246674833

---
+++ precache-manifest.c38399ee91a2ee18e9ce54f4337be924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5a50004e937f60c88ba5e3b0ed7afc",
+    "url": "/Tony3898/star_match_game/index.html"
+  },
+  {
+    "revision": "bc9f51e108e94cc6a317",
+    "url": "/Tony3898/star_match_game/static/css/main.966e19ac.chunk.css"
+  },
+  {
+    "revision": "0b12315e038a03612ec0",
+    "url": "/Tony3898/star_match_game/static/js/2.ff7328ed.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Tony3898/star_match_game/static/js/2.ff7328ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9f51e108e94cc6a317",
+    "url": "/Tony3898/star_match_game/static/js/main.b75bb2c1.chunk.js"
+  },
+  {
+    "revision": "aadf5f7cd74c47e4eedc",
+    "url": "/Tony3898/star_match_game/static/js/runtime-main.e82ef4c8.js"
+  }
+]);
